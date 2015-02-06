35b538466eb39e211d7b9b6961a73418654c2a1a
Nathan Charles <ncharles@gmail.com>
2015-02-06 13:20:56 -0500

    doc

--- misc.py
+++ misc.py
@@ -1,13 +1,8 @@
 import numpy as np
-#significant = lambda x,figures=3 : round(x,figures-int(np.floor(np.log10(x))))
-def significant(number,figures=3):
-    if number == 0.:
-        return number
-    order = np.ceil(np.log10(abs(number))).astype(int)
-    if order > figures:
-        return int(round(number,figures-order))
-    else:
-        return round(number,figures-order)
+def latexify(s):
+    s = s.replace('%', '\\%')
+    s = s.replace('$', '\\$')
+    return s
 
 def heatmap(list_like):
     mangled_a = []
@@ -18,10 +13,42 @@ def heatmap(list_like):
     data = np.rot90(data,3)
     data = np.fliplr(data)
     return data
+
 def module_temp(irradiance, weather_data):
     #todo: Maybe Sandia Module Temperature instead?                                                                                                                           
-    #TamizhMani 2003
+    """Module Temperature Calculation
+    :cite:`Tamizhmani2003`
+
+    Args:
+        irradiance (float): W/m**2
+        weather_data (dict): wind speed in m/s, ambient temp in C
+
+    Returns:
+        (float): temperature
+    """
+
     t_amb = float(weather_data["Dry-bulb (C)"])
     wind_ms = float(weather_data['Wspd (m/s)'])
     t_module = .945*t_amb + .028*irradiance - 1.528*wind_ms + 4.3
     return t_module
+
+#significant = lambda x,figures=3 : round(x,figures-int(np.floor(np.log10(x))))
+def significant(number,figures=3):
+    """Round to significant figures
+
+    Args:
+        number (float):
+        figures (int): figures of significance
+
+    Returns:
+        (float) or (int) if order of magnitude is greater than significance
+    """
+
+    if number == 0.:
+        return number
+    order = np.ceil(np.log10(abs(number))).astype(int)
+    if order > figures:
+        return int(round(number,figures-order))
+    else:
+        return round(number,figures-order)
+
