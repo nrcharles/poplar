66ccc8295c4661ff9882dbdb7718e3ff93a3fa80
Nathan Charles <ncharles@gmail.com>
2015-02-07 14:03:29 -0500

    style and doc

--- misc.py
+++ misc.py
@@ -1,26 +1,34 @@
 import numpy as np
+
+
 def latexify(s):
     s = s.replace('%', '\\%')
     s = s.replace('$', '\\$')
     return s
+
 
 def heatmap(list_like):
     mangled_a = []
     for i in range(0, len(list_like), 24):
         mangled_a.append(list_like[i:i+23])
     data = np.array(mangled_a)
-    #data = np.flipud(data)
-    data = np.rot90(data,3)
+    # data = np.flipud(data)
+    data = np.rot90(data, 3)
     data = np.fliplr(data)
     return data
 
+
 def module_temp(irradiance, weather_data):
-    #todo: Maybe Sandia Module Temperature instead?                                                                                                                           
+    # todo: Maybe Sandia Module Temperature instead?
     """Module Temperature Calculation
     :cite:`Tamizhmani2003`
 
+    .. math::
+        T_{module}(^{\circ}C ) = 0.943\cdot T_{ambient} + 0.028\cdot I_{total}
+        - 1.528 \cdot WindSpeed + 4.3
+
     Args:
-        irradiance (float): W/m**2
+        irradiance (float): W/m^2
         weather_data (dict): wind speed in m/s, ambient temp in C
 
     Returns:
@@ -32,8 +40,9 @@ def module_temp(irradiance, weather_data):
     t_module = .945*t_amb + .028*irradiance - 1.528*wind_ms + 4.3
     return t_module
 
-#significant = lambda x,figures=3 : round(x,figures-int(np.floor(np.log10(x))))
-def significant(number,figures=3):
+
+# significant = lambda x,figures=3: round(x,figures-int(np.floor(np.log10(x))))
+def significant(number, figures=3):
     """Round to significant figures
 
     Args:
@@ -48,7 +57,6 @@ def significant(number,figures=3):
         return number
     order = np.ceil(np.log10(abs(number))).astype(int)
     if order > figures:
-        return int(round(number,figures-order))
+        return int(round(number, figures-order))
     else:
-        return round(number,figures-order)
-
+        return round(number, figures-order)
