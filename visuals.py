f0c798313415352cee2aedceb0c1995f56a8dd6e
Nathan Charles <ncharles@gmail.com>
2015-02-22 18:44:06 -0500

    labeling clarity

--- visuals.py
+++ visuals.py
@@ -41,47 +41,36 @@ def report(domain, figname='SHS', title=None):
     td = domain.details()
     soc_frequency = fig.add_subplot(321)
     soc_frequency.set_xlabel('SoC')
-    soc_frequency.set_ylabel('Frequency')
+    soc_frequency.set_ylabel('Hourly Frequency')
     soc_frequency.set_title('Normalized SoC Histogram')
-    pp = np.array(domain.state_series)
+    soc_log = domain.soc_log()
+    print len(domain.state_series)
+    print len(soc_log)
+    pp = np.array(soc_log)
     pp.sort()
     fit = stats.norm.pdf(pp, np.mean(pp), np.std(pp))
 
-    soc_frequency.hist(domain.state_series, 40, normed=True)
+    soc_frequency.hist(soc_log, 40, normed=True)
     soc_frequency.plot(pp, fit)
 
     storage_soc = fig.add_subplot(322)
     storage_soc.set_xlabel('day')
     storage_soc.set_ylabel('hour')
     storage_soc.set_title('Storage State of Charge')
-    soc = storage_soc.imshow(heatmap(domain.state_series), aspect='auto')
+    soc = storage_soc.imshow(heatmap(soc_log), aspect='auto')
     soc_bar = fig.colorbar(soc)
     soc_bar.set_label('%')
 
-    demand_profile = fig.add_subplot(323)
-    demand_profile.set_xlabel('day')
-    demand_profile.set_ylabel('hour')
-    demand_profile.set_title('Demand Profile')
-    dp = demand_profile.imshow(heatmap(domain.l), aspect='auto')
-    dp_bar = fig.colorbar(dp)
-    dp_bar.set_label('W')
+    d_soc_frequency = fig.add_subplot(323)
+    d_soc_frequency.set_xlabel('SoC')
+    d_soc_frequency.set_ylabel('Daily Frequency')
+    d_soc_frequency.set_title('Normalized SoC Histogram')
+    d_pp = [np.mean(soc_log[i:i+23]) for i in range(0, 365*24, 24)]
+    d_pp.sort()
+    fit2 = stats.norm.pdf(d_pp, np.mean(d_pp), np.std(d_pp))
 
-    generator_o = fig.add_subplot(324)
-    generator_o.set_xlabel('day')
-    generator_o.set_ylabel('hour')
-    generator_o.set_title('Generator Output')
-    gp = generator_o.imshow(heatmap(domain.g), aspect='auto')
-    gp_bar = fig.colorbar(gp)
-    gp_bar.set_label('W')
-
-    s_constraint = fig.add_subplot(325)
-    s_constraint.set_xlabel('day')
-    s_constraint.set_ylabel('hour')
-    s_constraint.set_title('Domain Constraint')
-    bc = s_constraint.imshow(heatmap(domain.d), aspect='auto')
-    # cmap = plt.cm.Greys_r)
-    b4 = fig.colorbar(bc)
-    b4.set_label('W')
+    d_soc_frequency.hist(d_pp, 40, normed=True)
+    d_soc_frequency.plot(d_pp, fit2)
 
     figure_str = ("\\begin{figure}\n"
                   "\\centering\n"
@@ -104,26 +93,7 @@ def report(domain, figname='SHS', title=None):
                   "\\end{tabular}\n"
                   "\\end{table}\n")
 
-    domain_graph = fig.add_subplot(326)
-    G = domain.graph()
-    # pos = nx.spectral_layout(G)
-    pos = nx.spring_layout(G)
-    lpos = {}
-    for i in pos.iterkeys():
-        lpos[i] = pos[i] + [0, .15]
-    nx.draw_networkx_nodes(G, pos=pos, ax=domain_graph, node_size=200)
-    nx.draw_networkx_edges(G, pos=pos, ax=domain_graph)
-    ts = nx.draw_networkx_labels(G, pos=lpos, font_size=7)  # rotation=45)
 
-    for key in ts.iterkeys():
-        ts[key].set_rotation(45)
-
-    p = 1.2
-    x1, x2 = domain_graph.get_xlim()
-    y1, y2 = domain_graph.get_ylim()
-    domain_graph.set_xlim(x1*p, x2*p)
-    domain_graph.set_ylim(y1*p, y2*p)
-    domain_graph.axis('off')
     fig.tight_layout()
     fig.savefig('%s.pdf' % figname)
     return table_str, figure_str
@@ -141,23 +111,20 @@ def multi_report(domain, figname='SHS', title=None):
         title = figname
     fig = plt.figure(figsize=(8.5, 11))
     td = domain.details()
-    soc_frequency = fig.add_subplot(321)
-    soc_frequency.set_xlabel('SoC')
-    soc_frequency.set_ylabel('Frequency')
-    soc_frequency.set_title('Normalized SoC Histogram')
-    pp = np.array(domain.log_dict_to_list('demand'))
-    pp.sort()
-    fit = stats.norm.pdf(pp, np.mean(pp), np.std(pp))
-    print len(domain.log_dict_to_list('demand'))
-
-    soc_frequency.hist(domain.log_dict_to_list('demand'), 40, normed=True)
-    soc_frequency.plot(pp, fit)
+    s_constraint = fig.add_subplot(321)
+    s_constraint.set_xlabel('day')
+    s_constraint.set_ylabel('hour')
+    s_constraint.set_title('Domain Credits')
+    bc = s_constraint.imshow(heatmap(domain.log_dict_to_list('credits')), aspect='auto')
+    # cmap = plt.cm.Greys_r)
+    b4 = fig.colorbar(bc)
+    b4.set_label('Wh')
 
     storage_soc = fig.add_subplot(322)
     storage_soc.set_xlabel('day')
     storage_soc.set_ylabel('hour')
-    storage_soc.set_title('Storage State of Charge')
-    soc = storage_soc.imshow(heatmap(domain.log_dict_to_list('demand')), aspect='auto')
+    storage_soc.set_title('Domain Debits')
+    soc = storage_soc.imshow(heatmap(domain.log_dict_to_list('debits')), aspect='auto')
     soc_bar = fig.colorbar(soc)
     soc_bar.set_label('%')
 
@@ -167,24 +134,24 @@ def multi_report(domain, figname='SHS', title=None):
     demand_profile.set_title('Demand Profile')
     dp = demand_profile.imshow(heatmap(domain.log_dict_to_list('demand')), aspect='auto')
     dp_bar = fig.colorbar(dp)
-    dp_bar.set_label('W')
+    dp_bar.set_label('Wh')
 
     generator_o = fig.add_subplot(324)
     generator_o.set_xlabel('day')
     generator_o.set_ylabel('hour')
-    generator_o.set_title('Generator Output')
+    generator_o.set_title('Source Output')
     gp = generator_o.imshow(heatmap(domain.log_dict_to_list('source')), aspect='auto')
     gp_bar = fig.colorbar(gp)
-    gp_bar.set_label('W')
+    gp_bar.set_label('Wh')
 
     s_constraint = fig.add_subplot(325)
     s_constraint.set_xlabel('day')
     s_constraint.set_ylabel('hour')
-    s_constraint.set_title('Domain Constraint')
+    s_constraint.set_title('Domain Balance')
     bc = s_constraint.imshow(heatmap(domain.log_dict_to_list('balance')), aspect='auto')
     # cmap = plt.cm.Greys_r)
     b4 = fig.colorbar(bc)
-    b4.set_label('W')
+    b4.set_label('Wh')
 
     figure_str = ("\\begin{figure}\n"
                   "\\centering\n"
