1d0729d34addca7d79374e08de905e03fa993104
Nathan Charles <ncharles@gmail.com>
2015-02-26 15:37:06 -0500

    Fan

--- poplar/loads.py
+++ poplar/loads.py
@@ -33,7 +33,7 @@ def simple_profile(dt, mult=1.):
     return LOAD_PROFILE[offset] * mult
 
 
-def load(filename=None):
+def _load(filename=None):
     BD = {}
     if filename is None:
         filename = env.SRC_PATH + '/bd_hist_profile.csv'
@@ -47,7 +47,12 @@ def load(filename=None):
 
 
 class Load(Device):
+    """Load primative object.
+
+    This is the base object that all loads inherit.
+    """
     def bid(self):
+        """Returns: (object): bid for energy"""
         e = self.needsenergy()
         if e:
             return Bid(id(self), e, self.buy_kwh())
@@ -55,20 +60,31 @@ class Load(Device):
             return None
 
     def needsenergy(self):
+        """Returns: (float): energy need"""
         key = env.time
         if key not in self.balance:
             self.balance[key] = self.demand(key)
         return self.balance[key]
 
     def power_io(self, energy):
+        """Energy transfer.
+
+        Args:
+            energy (float): in wH.
+
+        Returns: i
+            (float): energy still needed.
+        """
         key = env.time
         self.balance[key] += energy
         return self.balance[key]
 
     def buy_kwh(self):
+        """Returns: (float) value of kwh."""
         return self.value_kwh()
 
     def enabled(self):
+        """Returns: (float) total energy load has actually used."""
         td = self.total() * env.total_time/self.interval
         sf = sum(self.balance.values())
         return abs(td - sf)
@@ -77,10 +93,12 @@ class Load(Device):
         return self.per_kwh
 
     def droopable(self):
+        """Returns: (float) droopable ratio of desired energy."""
         return 0.
 
 
 class Annual(Load):
+
     """Annual Weather Data Nominilized to 1 kWH annual.
 
     This is class is based on typical Bangladesh grid loads. The data was mined
@@ -92,9 +110,11 @@ class Annual(Load):
         mult (float): scaling factor
         year (int): year
     """
+
     def __init__(self, mult=71.4, year=2013):
-        """
-        Parameters:
+        """Initialize.
+
+        Args:
             mult (float): scaling factor (default 71.4)
             year (int): (default 2013)
 
@@ -105,7 +125,7 @@ class Annual(Load):
         self.droop_ratio = 0.
         self.year = year
         self.per_kwh = 0.07
-        self.data = load()
+        self.data = _load()
         self.small_id = SMALL_ID.next(type(self))
         self.balance = {}
         self.detail = None
@@ -135,7 +155,7 @@ class Annual(Load):
     __call__ = demand
 
     def total(self):
-        """Total Load for Year
+        """Total load for year.
 
         >>> BD = Annual(100.)
         >>> round(BD.total(), 1)
@@ -154,9 +174,68 @@ class Annual(Load):
         return self.detail
 
 
+class FanLoad(Load):
+
+    """Fan Load.
+
+    Atributes:
+        wattage (float): in negative wH.
+        thermostat (float): setting fan turns on in (C).
+        per_kwh (float): value of kWh.
+    """
+
+    def __init__(self, wattage, thermostat=28.):
+        """Initialize.
+
+        Args:
+            wattage (float): W
+            thermostat (float): W
+        """
+        self.wattage = -wattage
+        self.thermostat = thermostat
+        self.per_kwh = 0.075
+        self.dmnd = {}
+        self.balance = {}
+        self.interval = 24*265.
+
+    def demand(self, key):
+        """Demand returns (float) wH energy demand for (key)."""
+        if key not in self.dmnd:
+            if float(env.weather[key]["Dry-bulb (C)"]) > self.thermostat:
+                self.dmnd[key] = self.wattage
+            else:
+                self.dmnd[key] = 0.
+
+        return self.dmnd[key]
+
+    def total(self):
+        return sum(self.dmnd.values())
+
+    __call__ = demand
+
+    def __repr__(self):
+        return '%s W Fan' % self.wattage
+
+
 class DailyLoad(Load):
-    """Spline interpolated Load Profile"""
+
+    """Spline interpolated Load Profile.
+
+    Attributes:
+        per_kwh (float): value of energy.
+    """
+
     def __init__(self, hours, loads, kind='cubic', name=''):
+        """Initilize.
+
+        len(hours) == len(loads)
+
+        Args:
+            hours (list): of times (float) in hours.
+            loads (list): of total loads at hour (float) wH.
+            kind  (str): interpolation method default (cubic).
+            name (str):
+        """
         self.profile = interp1d(hours, loads, kind=kind)
         self.name = name
         self.classification = "load"
@@ -168,6 +247,7 @@ class DailyLoad(Load):
         self.interval = 24.
 
     def demand(self, dt):
+        """Return (float) energy demand wH for (datetime)."""
         return self.profile(dt.hour + dt.minute/60.)
 
     def total(self):
@@ -176,18 +256,30 @@ class DailyLoad(Load):
     __call__ = demand
 
     def __repr__(self):
-        return "%s %s, %s wH Daily" % (self.name, self.small_id, round(self.total(), 1))
+        return "%s %s, %s wH Daily" % (self.name, self.small_id,
+                                       round(self.total(), 1))
 
 times = np.array(range(0, 49))/2.
 spline_profile = DailyLoad(times, np.array(LOAD_PROFILE)*17)
 
+
 def tv(wattage=20):
+    """TV load.
+
+    Args:
+        wattage (float): base load
+
+    Returns:
+        (object) : DailyLoad object with TV profile
+    """
+    # todo: do i really want this as a function?
+
     l = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
                   -1, -1, -1, -1, 0, 0])
-    h = range(25) # to get 24 hour interpolation
+    h = range(25)  # to get 24 hour interpolation
     tv_inst = DailyLoad(h, l*wattage, kind='linear', name='12" B&W TV')
-    tv_inst.per_kwh = .08
-    return  tv_inst
+    tv_inst.per_kwh = .09
+    return tv_inst
 
 TV = tv(20)
 
@@ -221,6 +313,7 @@ def quantify_load(load, solar_gen):
     met_hours = sum(d >= 0)
     unmet_hours = sum(d < 0)
     return met_hours/unmet_hours
+
 
 if __name__ == '__main__':
     import doctest
