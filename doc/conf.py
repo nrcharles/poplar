35b5ac35c5a7fadb5f8252d1a9a8b06c58666ab7
Nathan Charles <ncharles@gmail.com>
2015-02-26 15:25:46 -0500

    path

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../poplar/'))
 
 # -- General configuration ------------------------------------------------
 
@@ -29,7 +29,8 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.bibtex',
-              'sphinx.ext.graphviz', 'sphinx.ext.mathjax']
+              'sphinx.ext.graphviz', 'sphinx.ext.mathjax',
+              'sphinxcontrib.programoutput']
 
 #autodoc_default_flags = ['members', 'special-members']
 
